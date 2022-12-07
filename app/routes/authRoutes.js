20a8e2f837e5d92f16c42ca1c8e757db7270e7b5
Moh Qoming Zaki <mizakighun@gmail.com>
2022-12-07 19:58:47 +0700

    add route login

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -2,5 +2,8 @@ const router = require('express').Router();
 const {register} = require('../controllers/authController');
 
 router.post('/auth/register', register);
+router.get('/auth/login', (req, res) => {
+    res.send('Login');
+});
 
 module.exports = router;

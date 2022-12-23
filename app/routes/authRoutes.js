912774ac045fedcc66e9230e14674c224d889a7e
Moh Qoming Zaki <mizakighun@gmail.com>
2022-12-23 20:30:14 +0700

    add feature updateProfile

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -1,10 +1,11 @@
 const router = require('express').Router();
-const {login, register, verify, getProfile, loginGoogle} = require('../controllers/authController');
+const {login, register, verify, getProfile, loginGoogle, updateProfile} = require('../controllers/authController');
 
 router.post('/auth/login', login);
 router.post('/auth/register', register);
 router.get('/auth/verify', verify);
 router.get('/auth/getProfile', getProfile);
 router.get('/auth/google-login' , loginGoogle);
+router.put('/auth/updateProfile', auth, upload.single('image'), updateProfile);
 
 module.exports = router;

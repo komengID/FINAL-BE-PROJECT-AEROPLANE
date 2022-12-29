bdcc3377a79a7c62da9dfd5f05f078b3944f6e78
afanfian <afanfian14@gmail.com>
2022-12-29 10:45:55 +0700

    fix: update auth di route

--- app/routes/authRoutes.js
+++ app/routes/authRoutes.js
@@ -1,5 +1,5 @@
 const router = require('express').Router();
-const {login, register, verify, getAllUsers, getProfile, loginGoogle, updateProfile} = require('../controllers/authController');
+const { login, register, verify, getAllUsers, getProfile, loginGoogle, updateProfile } = require('../controllers/authController');
 
 // middleware
 const auth = require('../../middleware/authentication');
@@ -8,8 +8,8 @@ const upload = require('../../libs/uploader');
 router.post('/auth/login', login);
 router.post('/auth/register', register);
 router.post('/auth/verify', verify);
-router.get('/auth/allusers', getAllUsers);
-router.get('/auth/profile', getProfile);
+router.get('/auth/allusers', auth, getAllUsers);
+router.get('/auth/profile', auth, getProfile);
 router.get('/auth/google-login', loginGoogle);
 router.put('/auth/profile', upload.single('image'), updateProfile);
 

3a78e9811bed0609b8855ba3f011ab0afa5a74ba
Selly Melananda <melanand4@gmail.com>
2022-12-30 14:22:50 +0700

    feat data db-seeds

---
+++ db/data/passangersData.js
@@ -0,0 +1,94 @@
+const passangersData =[
+    {
+      "id": 7,
+      "passenger_name": "Selly Bintang Melananda",
+      "nik": "3510023553000982",
+      "createdAt": "2022-12-29T07:50:47.230Z",
+      "updatedAt": "2022-12-29T07:50:47.230Z"
+    },
+    {
+      "id": 8,
+      "passenger_name": "Fian Awamiry Maulana",
+      "nik": "3510023553000987",
+      "createdAt": "2022-12-29T17:37:41.905Z",
+      "updatedAt": "2022-12-29T17:37:41.905Z"
+    },
+    {
+      "id": 13,
+      "passenger_name": "fian",
+      "nik": "12345678",
+      "createdAt": "2022-12-29T19:18:02.317Z",
+      "updatedAt": "2022-12-29T19:18:02.317Z"
+    },
+    {
+      "id": 15,
+      "passenger_name": "heri",
+      "nik": "5025201035",
+      "createdAt": "2022-12-29T19:25:46.629Z",
+      "updatedAt": "2022-12-29T19:25:46.629Z"
+    },
+    {
+      "id": 16,
+      "passenger_name": "fian",
+      "nik": "089515776243",
+      "createdAt": "2022-12-29T19:27:19.447Z",
+      "updatedAt": "2022-12-29T19:27:19.447Z"
+    },
+    {
+      "id": 17,
+      "passenger_name": "fian",
+      "nik": "3508161308020003",
+      "createdAt": "2022-12-29T19:30:45.562Z",
+      "updatedAt": "2022-12-29T19:30:45.562Z"
+    },
+    {
+      "id": 18,
+      "passenger_name": "fian",
+      "nik": "3508161308020003",
+      "createdAt": "2022-12-29T19:33:58.468Z",
+      "updatedAt": "2022-12-29T19:33:58.468Z"
+    },
+    {
+      "id": 19,
+      "passenger_name": "fian",
+      "nik": "12345678",
+      "createdAt": "2022-12-29T19:41:18.465Z",
+      "updatedAt": "2022-12-29T19:41:18.465Z"
+    },
+    {
+      "id": 32,
+      "passenger_name": "heri",
+      "nik": "5025201035",
+      "createdAt": "2022-12-29T21:02:17.102Z",
+      "updatedAt": "2022-12-29T21:02:17.102Z"
+    },
+    {
+      "id": 38,
+      "passenger_name": "Fian Awamiry Maulana",
+      "nik": "5025201035",
+      "createdAt": "2022-12-30T03:38:44.242Z",
+      "updatedAt": "2022-12-30T03:38:44.242Z"
+    },
+    {
+      "id": 43,
+      "passenger_name": "Zaki Aja",
+      "nik": "3510023553000392",
+      "createdAt": "2022-12-30T06:16:14.325Z",
+      "updatedAt": "2022-12-30T06:16:14.325Z"
+    },
+    {
+      "id": 44,
+      "passenger_name": "Agnes Nababan",
+      "nik": "351002355300838392",
+      "createdAt": "2022-12-30T06:16:29.873Z",
+      "updatedAt": "2022-12-30T06:16:29.873Z"
+    },
+    {
+      "id": 45,
+      "passenger_name": "Albi",
+      "nik": "351002355300888392",
+      "createdAt": "2022-12-30T06:16:47.149Z",
+      "updatedAt": "2022-12-30T06:16:47.149Z"
+    }
+]
+module.exports= passangersData
